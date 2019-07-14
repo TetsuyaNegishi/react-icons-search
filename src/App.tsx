3ef518f76a7863cca207ea911ff4dca852d6be14
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2019-07-14 16:46:10 +0900

    show search result icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useCallback, useMemo } from 'react';
 import './App.css';
+import * as Icons from "react-icons/all";
+import TextField from '@material-ui/core/TextField';
 
 const App: React.FC = () => {
+  const [value, setValue] = useState('');
+
+  const handleOnChangeInput = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }, [setValue])
+
+  const data = useMemo(() => {
+    if(!value) {
+      return []
+    }
+    return Object.entries(Icons).filter(([name]) => name.toLowerCase().search(value.toLowerCase()) !== -1)
+  }, [value])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextField
+        id="standard-search"
+        label="Search field"
+        type="search"
+        fullWidth
+        onChange={handleOnChangeInput}
+        margin="normal"
+      />
+      <div>
+        {data.map(([name, Icon]) => <div key={name}><Icon />{name}</div>)}
+      </div>
     </div>
   );
 }
