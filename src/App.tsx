3f793dbf03278bad4243c68e8a849f4e37914543
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2019-07-21 19:46:31 +0900

    add emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/core';
 import React, { useState, useCallback, useMemo } from 'react';
 import './App.css';
 import * as Icons from "react-icons/all";
@@ -7,14 +8,14 @@ const { Header, Content } = Layout;
 
 const iconsList = Object.entries(Icons);
 
-const iconBoxStyle: React.CSSProperties = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(auto-fit, 150px)',
-  gap: '20px',
-  justifyContent: 'center',
+const iconBoxCss = css`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 150px);
+  gap: 20px;
+  justify-content: center;
 
-  padding: '10px 0',
-}
+  padding: 10px 0;
+`;
 
 const App: React.FC = () => {
   const [value, setValue] = useState('');
@@ -35,7 +36,7 @@ const App: React.FC = () => {
       <Header><h1 style={{color: 'white'}}>react-icons-search</h1></Header>
       <Content style={{padding: '20px 50px'}}>
         <Input type="search" prefix={<Icon type="search" />} size="large" onChange={handleOnChangeInput} />
-        <div style={iconBoxStyle}>
+        <div css={iconBoxCss}>
           {data.map(([name, Icon]) => (
             <Card key={name} style={{textAlign: 'center'}} hoverable size="small" cover={<div style={{paddingTop: '10px'}}><Icon size={50} /></div>}>
               <Card.Meta title={name} />
